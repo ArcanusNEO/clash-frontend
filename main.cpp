db6072f1142dabeb3f40ce91700b30c7e4286b04
ArcanusNEO <lucasding@foxmail.com>
2022-01-17 20:22:21 +0800

    。

--- main.cpp
+++ main.cpp
@@ -4,20 +4,19 @@
 #include <QLocale>
 #include <QTranslator>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "clash-frontend_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
+  QTranslator translator;
+  const QStringList uiLanguages = QLocale::system().uiLanguages();
+  for (const QString& locale : uiLanguages) {
+    const QString baseName = "clash-frontend_" + QLocale(locale).name();
+    if (translator.load(":/i18n/" + baseName)) {
+      a.installTranslator(&translator);
+      break;
     }
-    MainWindow w;
-    w.show();
-    return a.exec();
+  }
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
